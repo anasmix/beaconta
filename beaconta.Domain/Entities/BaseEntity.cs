7669ef2d6b69b344ccc6294580349a03d59c404f
anas_ <anas_@ANAS_SURFACE>
2025-09-13 18:42:15 +0300

    v2

--- beaconta.Domain/Entities/BaseEntity.cs
+++ beaconta.Domain/Entities/BaseEntity.cs
@@ -2,6 +2,7 @@
 {
     public abstract class BaseEntity
     {
+        public int Id { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public string CreatedBy { get; set; } = string.Empty;
 
--- beaconta.Domain/Entities/Permission.cs
+++ beaconta.Domain/Entities/Permission.cs
@@ -1,8 +1,7 @@
 ﻿namespace beaconta.Domain.Entities
 {
     public class Permission : BaseEntity
-    {
-        public int Id { get; set; }
+    { 
         public string Key { get; set; } = string.Empty;      // مثل: students.view
         public string Name { get; set; } = string.Empty;     // مثل: عرض الطلاب
         public string Category { get; set; } = string.Empty; // مثل: الطلاب
